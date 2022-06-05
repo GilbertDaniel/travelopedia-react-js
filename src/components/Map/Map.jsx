e7bc4bbdbf0e21440e1edf540a8e4ef4ef0c0668
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-05 22:11:23 +0630

    Implementing Google Map API

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './style.js';
 const Map = () => {
+    const matches = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+    const coords = { lat: 0, lng: 0 }
     return (
-        <div>Map</div>
+        // Important! Always set the container height explicitly
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyA1IflOa3odAfkQ_n58DRz7UDs1XojE4xY' }}
+                defaultCenter={coords}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                option={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
-export default Map
+export default Map;
