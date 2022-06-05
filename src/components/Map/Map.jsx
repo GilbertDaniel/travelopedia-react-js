e2ee5e2e4e3fcbaccb15ced513eec3f3b26ce28f
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-06 01:10:22 +0630

    Showing Places on the Map with Marker

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,10 +5,9 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './style.js';
-const Map = ({setCoordinates, setBounds, coordinates}) => {
-    const matches = useMediaQuery('(min-width:600px)');
+const Map = ({setCoordinates, setBounds, coordinates, places}) => {
+    const isDeskTop = useMediaQuery('(min-width:600px)');
     const classes = useStyles();
-    const coords = { lat: 0, lng: 0 }
     return (
         // Important! Always set the container height explicitly
         <div className={classes.mapContainer}>
@@ -24,6 +23,35 @@ const Map = ({setCoordinates, setBounds, coordinates}) => {
                 }}
                 onChildClick={''}
             >
+                {places?.map((place,i) => (
+                    <div
+                     className={classes.markerContainer}
+                    lat={Number(place.latitude)}
+                    lng={Number(places.longitude)}
+                    key={i}
+                    >
+                        {
+                            !isDeskTop ? (
+                                <LocationOnOutlinedIcon
+                                    color='primary'
+                                    fontSize='large'
+                                />
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="sbtitle2" gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                        alt={place.name}
+                                    />
+                                    <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
 
             </GoogleMapReact>
         </div>
