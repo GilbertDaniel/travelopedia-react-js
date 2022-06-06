70e498fee9e2fdfe7960551ff36adf8781e726ce
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-06-06 15:21:38 +0630

    Remove AutoComplete Search Feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,19 +13,6 @@ const Header = () => {
             <Typography variant="h5" className={classes.title}>
               Travelopedia
             </Typography>
-            <Box display="flex">
-              <Typography variant="h6" className={classes.title}>
-                Explore new places
-              </Typography>
-              {/* <Autocomplete> */}
-                <div className={classes.search}>
-                  <div className={classes.searchIcon}>
-                    <SearchIcon />
-                  </div>
-                  <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-                </div>
-              {/* </Autocomplete> */}
-            </Box>
           </Toolbar>
         </AppBar>
       );
